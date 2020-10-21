2a8a9a65bbbb917be137e910c9f74df0859b5c65
Shekhar-Patil <patilshekhar900@gmail.com>
2020-10-21 08:58:53 +0530

    Added JWT and devise gem, devise routes

---
+++ app/controllers/api/v1/partners/registrations_controller.rb
@@ -0,0 +1,33 @@
+class Api::V1::Partners::RegistrationsController < Devise::SessionsController
+  respond_to :json
+  def create
+    user = User.new user_params
+    if user.save
+      render json: {
+        messages: "Sign Up Successfully",
+        is_success: true,
+        data: {user: user}
+      }, status: :ok
+    else
+      render json: {
+        messages: "Sign Up Failded",
+        is_success: false,
+        data: { errors: user.errors.full_messages }
+      }, status: :unprocessable_entity
+    end
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:email, :password, :password_confirmation)
+  end
+
+  def ensure_params_exist
+    return if params[:user].present?
+    render json: {
+        messages: "Missing Params",
+        is_success: false,
+        data: {}
+      }, status: :bad_request
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,24 @@
 Rails.application.routes.draw do
+  devise_for :users
+
+  namespace :api, defaults: {format: :json} do
+    namespace :v1 do
+      namespace :customers do
+        devise_scope :user do
+          post "sign_up", to: "registrations#create"
+        end
+      end
+
+      namespace :partners do
+        devise_scope :user do
+          post "sign_up", to: "registrations#create"
+        end
+      end
+
+      devise_scope :user do
+        post "sign_in", to: "sessions#create"
+      end
+    end
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
